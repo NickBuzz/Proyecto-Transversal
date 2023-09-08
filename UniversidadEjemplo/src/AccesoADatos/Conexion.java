be47da11d3fa2d855445d230332a3469a63a0b9b
Tania0Arc3 <arcetaniia5@gmail.com>
2023-09-08 00:20:15 -0300

    cree la conexion al proyecto

--- UniversidadEjemplo/src/AccesoADatos/Conexion.java
+++ UniversidadEjemplo/src/AccesoADatos/Conexion.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package AccesoADatos;
 
-/**
- *
- * @author Oking
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import static java.util.Date.UTC;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
 public class Conexion {
     
+    
+    private static final String URL = "jdbc:mariadb://localhost:3306/proyecto transversal";
+    private static final String USUARIO= "root";
+    private static final String PASSWORD = "";
+
+    private static Connection connection;
+
+   private Conexion (){}
+   public static Connection getConexion(){
+       if(connection == null){
+           try {
+               Class.forName("org.mariadb.jdbc.Driver");
+               connection = DriverManager.getConnection(URL,USUARIO,PASSWORD);
+               
+           } catch (ClassNotFoundException ex) {
+              JOptionPane.showMessageDialog(null,"error al cargar el driver"+ex.getMessage());
+           
+           } catch (SQLException ex) {
+               JOptionPane.showMessageDialog(null, "Error de conexion"+ex.getMessage());
+           }
+           
+       }
+       return connection;
+   }
 }
