feda54194fede0b3e8568c2a53707037be04ebbb
Tania0Arc3 <arcetaniia5@gmail.com>
2023-09-08 11:30:31 -0300

    algo

--- UniversidadEjemplo/src/Entidades/Inscripcion.java
+++ UniversidadEjemplo/src/Entidades/Inscripcion.java
@@ -10,5 +10,58 @@ package Entidades;
  * @author Oking
  */
 public class Inscripcion {
+    private int idIncripcion;
+    private Alumno alumno;
+    private Materia materia;
+    private double nota;
+
+    public Inscripcion() {
+    }
+
+    public Inscripcion(int idIncripcion, Alumno alumno, Materia materia, double nota) {
+        this.idIncripcion = idIncripcion;
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public Inscripcion(Alumno alumno, Materia materia, double nota) {
+        this.alumno = alumno;
+        this.materia = materia;
+        this.nota = nota;
+    }
+
+    public int getIdIncripcion() {
+        return idIncripcion;
+    }
+
+    public void setIdIncripcion(int idIncripcion) {
+        this.idIncripcion = idIncripcion;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    
     
 }
