99eb9b621482dfb0346983acc0f5d28591da2c5d
Tania0Arc3 <arcetaniia5@gmail.com>
2023-09-24 23:58:20 -0300

    .

--- UniversidadEjemplo/src/Vistas/FormularioDeInscripcion.java
+++ UniversidadEjemplo/src/Vistas/FormularioDeInscripcion.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Vistas;
 
 import AccesoADatos.AlumnoData;
@@ -34,13 +30,7 @@ public class FormularioDeInscripcion extends javax.swing.JPanel {
         }
     };
 
-    /**
-     * Creates new form FormularioDeInscripcion
-     *
-     * @param ad
-     * @param md
-     * @param id
-     */
+  
     public FormularioDeInscripcion(AlumnoData ad, MateriaData md, InscripcionData id) {
         initComponents();
         this.ad = ad;
